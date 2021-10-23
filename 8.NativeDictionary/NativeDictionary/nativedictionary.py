1527dc2c922e0d933ced969e58e8b41784365ef7
Константин Доронин <admin@sib-fisher.ru>
2021-10-23 15:38:04 +0600

    Create nativedictionary.py

---
+++ 8.NativeDictionary/NativeDictionary/nativedictionary.py
@@ -0,0 +1,75 @@
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+
+    def hash_fun(self, value):
+        # в качестве value поступают строки!
+        index = len(value)%self.size
+        # всегда возвращает корректный индекс слота
+        return index
+
+    def seek_slot(self, key):
+        # находит индекс пустого слота для значения, или None
+        index = self.hash_fun(key)
+        if self.slots[index] is None:
+            return index
+        else:
+            i = 0
+            while i < self.size:
+                index += 1
+                if index >= self.size:
+                    index = index - self.size
+                if self.slots[index] is None:
+                    return index
+                else:
+                    i += 1
+            return None
+
+    def is_key(self, key):
+        # возвращает True если ключ имеется,
+        index = self.hash_fun(key)
+        if self.slots[index] == key:
+            return index
+        else:
+            i = 0
+            while i < self.size:
+                index += 1
+                if index >= self.size:
+                    index = index - self.size
+                if self.slots[index] == key:
+                    return True
+                elif self.slots[index] is None:
+                    return False
+                else:
+                    i += 1
+            return False
+
+    def put(self, key, value):
+        index = self.seek_slot(key)
+        self.slots[index] = key
+        self.values[index] = value
+        return index
+        # гарантированно записываем
+        # значение value по ключу key
+
+    def get(self, key):
+        # возвращает value для key,
+        # или None если ключ не найден
+        index = self.hash_fun(key)
+        if self.slots[index] == key:
+            return self.values[index]
+        else:
+            i = 0
+            while i < self.size:
+                index += 1
+                if index >= self.size:
+                    index = index - self.size
+                if self.slots[index] == key:
+                    return self.values[index]
+                elif self.slots[index] is None:
+                    return None
+                else:
+                    i += 1
+            return None
