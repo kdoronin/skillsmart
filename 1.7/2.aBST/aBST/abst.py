608a9203d6927df8ff44776684a1dd2286e9f610
Константин Доронин <admin@sib-fisher.ru>
2021-11-28 19:57:13 +0600

    abst

---
+++ 1.7/2.aBST/aBST/abst.py
@@ -0,0 +1,60 @@
+class aBST:
+
+    def __init__(self, depth):
+        # правильно рассчитайте размер массива для дерева глубины depth:
+        self.tree_size = 2**(depth + 1) - 1
+        self.Tree = [None] * self.tree_size  # массив ключей
+
+    def FindKeyIndex(self, key):
+        # ищем в массиве индекс ключа
+        node = self.Tree[0]
+        return self.find_key_index_step(key, 0)
+
+    def find_key_index_step(self, key, index):
+        if index >= self.tree_size:
+            return None
+        if self.Tree[index] is None:
+            return None
+        else:
+            if self.Tree[index] == key:
+                return index
+            elif key > self.Tree[index]:
+                if self.get_right_child(index) is not None:
+                    return self.find_key_index_step(key, 2*index + 2)
+                else:
+                    return None
+            else:
+                if self.get_left_child(index) is not None:
+                    return self.find_key_index_step(key, 2*index + 1)
+                else:
+                    return None
+
+    def get_left_child(self, index):
+        child_index = 2*index + 1
+        if child_index <= self.tree_size:
+            return self.Tree[child_index]
+        else:
+            return None
+
+    def get_right_child(self, index):
+        child_index = 2 * index + 2
+        if child_index <= self.tree_size:
+            return self.Tree[child_index]
+        else:
+            return None
+
+    def AddKey(self, key):
+        # добавляем ключ в массив
+        return self.add_one_step(key, 0)
+        # индекс добавленного/существующего ключа или -1 если не удалось
+
+    def add_one_step(self, key, index):
+        if index >= self.tree_size:
+            return -1
+        if self.Tree[index] is None:
+            self.Tree[index] = key
+            return True
+        elif key >= self.Tree[index]:
+            return self.add_one_step(key, 2*index + 2)
+        else:
+            return self.add_one_step(key, 2*index + 1)
