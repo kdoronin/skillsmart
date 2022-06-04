c5def2f19f7b126c3138072a1cf3a74ddbb5d1d6
Konstantin Doronin <admin@sib-fisher.ru>
2022-06-04 20:26:27 +0600

    fix 1.17

--- 2.a.3/1.17/1.17/Program.fs
+++ 2.a.3/1.17/1.17/Program.fs
@@ -13,6 +13,7 @@ let rec isIthChar = function
 // 17.3
 let rec occFromIth_rec = function
 | (s: string, n : int, c : char, i : int) when n >= String.length s -> i
+| (s: string, n : int, c : char, i : int) when n < 0 s -> i
 | (s: string, n : int, c : char, i : int) when s.[n] = c -> occFromIth_rec(s, n+1, c, i+1)
 | (s: string, n : int, c : char, i : int) -> occFromIth_rec(s, n+1, c, i)
 
