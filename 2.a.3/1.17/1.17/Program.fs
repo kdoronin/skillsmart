4cb340944c90059a2b0c9ce1942a8e713494162e
Konstantin Doronin <admin@sib-fisher.ru>
2022-06-04 20:30:17 +0600

    fix 1.17

--- 2.a.3/1.17/1.17/Program.fs
+++ 2.a.3/1.17/1.17/Program.fs
@@ -1,8 +1,8 @@
 ﻿// 17.1
 let rec pow = function
-| (s,1) -> printfn "%s" s
+| (s,1) -> printf "%s" s
 | (s,n) ->
-    printfn "%s" s
+    printf "%s" s
     pow(s, n-1)
 
 // 17.2
@@ -13,7 +13,7 @@ let rec isIthChar = function
 // 17.3
 let rec occFromIth_rec = function
 | (s: string, n : int, c : char, i : int) when n >= String.length s -> i
-| (s: string, n : int, c : char, i : int) when n < 0 s -> i
+| (s: string, n : int, c : char, i : int) when n < 0 -> i
 | (s: string, n : int, c : char, i : int) when s.[n] = c -> occFromIth_rec(s, n+1, c, i+1)
 | (s: string, n : int, c : char, i : int) -> occFromIth_rec(s, n+1, c, i)
 
