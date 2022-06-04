6b431c0c6a081aed206cc66d28d5740c71112fe5
Konstantin Doronin <admin@sib-fisher.ru>
2022-06-04 19:46:53 +0600

    add 1.17

---
+++ 2.a.3/1.17/1.17/Program.fs
@@ -0,0 +1,20 @@
+﻿// 17.1
+let rec pow = function
+| (s,0) -> ""
+| (s,n) -> printfn "%s" s
+           pow(s, n-1)
+
+// 17.2
+let rec isIthChar = function
+| (s : string,n : int,c : char) when s.[n] = c -> true
+| (s : string,n : int,c : char) -> false
+
+// 17.3
+let rec occFromIth_rec = function
+| (s: string, n : int, c : char, i : int) when n >= String.length s -> i
+| (s: string, n : int, c : char, i : int) when s.[n] = c -> occFromIth_rec(s, n+1, c, i+1)
+| (s: string, n : int, c : char, i : int) -> occFromIth_rec(s, n+1, c, i)
+
+
+let rec occFromIth = function
+| (s : string,n : int,c : char) -> occFromIth_rec(s,n,c,0)
